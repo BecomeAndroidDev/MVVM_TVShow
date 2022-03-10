db780124323af9b5a13005ffa64669510720c83f
ckonlyvk <ducluuthien@gmail.com>
2022-03-11 00:30:52 +0700

    Display details screen

--- source/app/src/main/java/com/hfad/tvshow/activities/TVShowDetailsActivity.java
+++ source/app/src/main/java/com/hfad/tvshow/activities/TVShowDetailsActivity.java
@@ -2,6 +2,7 @@ package com.hfad.tvshow.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.core.text.HtmlCompat;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.viewpager2.widget.ViewPager2;
@@ -9,7 +10,9 @@ import androidx.viewpager2.widget.ViewPager2;
 import android.content.Context;
 import android.content.Intent;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -24,13 +27,15 @@ import com.hfad.tvshow.models.TVShowDetails;
 import com.hfad.tvshow.responses.TVShowDetailsResponse;
 import com.hfad.tvshow.viewmodels.TVShowDetailsViewModel;
 
+import java.util.Locale;
+
 public class TVShowDetailsActivity extends AppCompatActivity {
     public static String EXTRA_TV_SHOW_ID = "id";
     public static String EXTRA_TV_SHOW_NAME = "name";
     public static String EXTRA_TV_SHOW_START_DATE = "start_date";
-    public static String EXTRA_TV_COUNTRY = "country";
-    public static String EXTRA_TV_NETWORK = "network";
-    public static String EXTRA_TV_STATUS = "status";
+    public static String EXTRA_TV_SHOW_COUNTRY = "country";
+    public static String EXTRA_TV_SHOW_NETWORK = "network";
+    public static String EXTRA_TV_SHOW_STATUS = "status";
 
     private ActivityTvshowDetailsBinding mBinding;
     private TVShowDetailsViewModel mViewModel;
@@ -46,6 +51,7 @@ public class TVShowDetailsActivity extends AppCompatActivity {
 
     private void doInitialization() {
         mViewModel = new ViewModelProvider(this).get(TVShowDetailsViewModel.class);
+        mBinding.imageBack.setOnClickListener(view -> onBackPressed());
         getTVShowDetails();
     }
 
@@ -61,6 +67,52 @@ public class TVShowDetailsActivity extends AppCompatActivity {
                     if(tvShowDetails.getPictures() != null) {
                         loadImageSlider(tvShowDetails.getPictures());
                     }
+                    mBinding.setTvShowImageURL(tvShowDetails.getImagePath());
+                    mBinding.imageTvShow.setVisibility(View.VISIBLE);
+
+                    mBinding.setDescription(String.valueOf(
+                            HtmlCompat.fromHtml(tvShowDetails.getDescription()
+                                    , HtmlCompat.FROM_HTML_MODE_COMPACT)
+                    ));
+                    mBinding.textDescription.setVisibility(View.VISIBLE);
+                    mBinding.textReadMore.setVisibility(View.VISIBLE);
+                    mBinding.textReadMore.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            if(mBinding.textReadMore.getText().toString().equals(
+                                    getResources().getString(R.string.read_more))) {
+                                mBinding.textDescription.setMaxLines(Integer.MAX_VALUE);
+                                mBinding.textDescription.setEllipsize(null);
+                                mBinding.textReadMore.setText(R.string.read_less);
+                            }
+                            else {
+                                mBinding.textDescription.setMaxLines(4);
+                                mBinding.textDescription.setEllipsize(TextUtils.TruncateAt.END);
+                                mBinding.textReadMore.setText(R.string.read_more);
+                            }
+                        }
+                    });
+
+                    mBinding.setRating(tvShowDetails.getRating());
+                    if(tvShowDetails.getGenres() != null) {
+                        mBinding.setGenre(tvShowDetails.getGenres()[0]);
+                    } else {
+                        mBinding.setGenre("N/A");
+                    }
+                    mBinding.setRuntime(tvShowDetails.getRuntime() + " Min");
+                    mBinding.viewDivider.setVisibility(View.VISIBLE);
+                    mBinding.layoutMisc.setVisibility(View.VISIBLE);
+                    mBinding.viewDivider2.setVisibility(View.VISIBLE);
+
+                    mBinding.buttonWebsite.setOnClickListener(view -> {
+                        Intent intent = new Intent(Intent.ACTION_VIEW);
+                        intent.setData(Uri.parse(tvShowDetails.getUrl()));
+                        startActivity(intent);
+                    });
+                    mBinding.buttonWebsite.setVisibility(View.VISIBLE);
+                    mBinding.buttonEpisodes.setVisibility(View.VISIBLE);
+
+                    loadBasicTVShowDetails();
                 }
             }
         });
@@ -117,14 +169,29 @@ public class TVShowDetailsActivity extends AppCompatActivity {
         }
     }
 
+    private void loadBasicTVShowDetails() {
+        Intent intent = getIntent();
+        mBinding.setTvShowName(intent.getStringExtra(EXTRA_TV_SHOW_NAME));
+        mBinding.setStartedDate(intent.getStringExtra(EXTRA_TV_SHOW_START_DATE));
+        mBinding.setStatus(intent.getStringExtra(EXTRA_TV_SHOW_STATUS));
+        mBinding.setNetworkCountry(
+                intent.getStringExtra(EXTRA_TV_SHOW_NETWORK) +" ("
+                        + intent.getStringExtra(EXTRA_TV_SHOW_COUNTRY) + " )");
+        
+        mBinding.tvShowName.setVisibility(View.VISIBLE);
+        mBinding.textNetworkCountry.setVisibility(View.VISIBLE);
+        mBinding.textStatus.setVisibility(View.VISIBLE);
+        mBinding.textStartedDate.setVisibility(View.VISIBLE);
+    }
+
     public static Intent newIntent(Context context, TVShow tvShow) {
         Intent intent = new Intent(context, TVShowDetailsActivity.class);
         intent.putExtra(EXTRA_TV_SHOW_ID, tvShow.getId());
         intent.putExtra(EXTRA_TV_SHOW_NAME, tvShow.getName());
         intent.putExtra(EXTRA_TV_SHOW_START_DATE, tvShow.getStartDate());
-        intent.putExtra(EXTRA_TV_COUNTRY, tvShow.getCountry());
-        intent.putExtra(EXTRA_TV_NETWORK, tvShow.getNetwork());
-        intent.putExtra(EXTRA_TV_STATUS, tvShow.getStatus());
+        intent.putExtra(EXTRA_TV_SHOW_COUNTRY, tvShow.getCountry());
+        intent.putExtra(EXTRA_TV_SHOW_NETWORK, tvShow.getNetwork());
+        intent.putExtra(EXTRA_TV_SHOW_STATUS, tvShow.getStatus());
         return intent;
     }
 }
